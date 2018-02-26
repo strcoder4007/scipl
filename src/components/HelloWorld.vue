f29ed3d704964af52676f70486522fa8ff65abdc
Shubham Singh <mynameshubham@gmail.com>
2018-02-27 03:37:58 +0400

    layout created

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="hello">
-      <div class = "row">
-          <div class="col-md-6" style="background-color: red; height: 40px;"></div>
-          <div class="col-md-6" style="background-color: blue; height: 40px;"></div>
-      </div>
-  </div>
+    <div class="hello">
+        <div class="row">
+            <div class="col-sm-12 col-md-3" style="background-color: #333; height: 100vh;">
+                
+            </div>
+            <div class="col-sm-12 col-md-9" style="background-color:  #222; height: 100vh;">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
