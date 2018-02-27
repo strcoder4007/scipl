1887d6c8caf657bba473aa07dc0a6b18f9839f32
Shubham Singh <mynameshubham@gmail.com>
2018-02-28 00:15:52 +0400

    up until sidebar's crazy behaviour

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="hello">
         <div class="row gutter-0">
-            <div class="col-sm-12 col-md-2" style="background-color: #333; min-height: 100vh; overflow: auto">
+            <div class="col-sm-12 col-md-2 sidebar">
                 <span class="brand">SC</span>
                 <div class="clearfix"></div>
                 <div class="seasonsClass" v-on:click="showSeasons = !showSeasons">Seasons</div>
                 <div v-if="showSeasons">
-                    <div v-on:click="toggleMatches(index)" v-for="(season, index) in seasons" :key='index' class="seasonsClass">{{ season.name }}<br>
-                        <div v-if="showMatches[index]">
-                            <div v-on:click="gotoMatchPage(index)" v-for="(match, idx) in matches[index]" v-bind:key='idx'>{{ match }}</div>
+                    <div v-on:click="showMatches = !showMatches" v-for="(season, index) in seasons" :key='index' class="seasonsClass">{{ season.name }}<br>
+                        <div v-if="showMatches">
+                            <div v-on:click="gotoMatchPage(index)" v-for="(match, idx) in matches[index]" v-bind:key='idx' class="matchClass">{{ match }}</div>
                         </div>
                     </div>
                 </div>
@@ -27,20 +27,14 @@
 import Papa from '../../node_modules/papaparse/papaparse.js'
 
 
-//hard-coding the size of matches array to 10
+//hard-coding the size of matches array to 10x4
 var showSeasons = false;
-var showMatches = new Array(10);
-for (var i = 0; i < showMatches.length; i++)
-    showMatches[i] = false;
+var showMatches = false;
 var matches = new Array(10);
 for (var i = 0; i < 10; i++) {
     matches[i] = new Array(90);
-    for (var j = 0; j < 10; j++)
+    for (var j = 0; j < 4; j++)
         matches[i].push("match");
-}
-var toggleMatches = function(myId) {
-    this.showMatches[myId] = !this.showMatches[myId];
-    alert(this.showMatches[myId]);
 }
 
 export default {
@@ -57,7 +51,7 @@ export default {
                 { "name": "2010" },
                 { "name": "2011" },
                 { "name": "2012" }
-            ], showSeasons, showMatches, matches, toggleMatches
+            ], showSeasons, showMatches, matches
         }
     }
 }
@@ -83,22 +77,36 @@ Papa.parse(file, {
     width: 100vw;
 }
 
+.sidebar {
+    background-color: #333; 
+    min-height: 100vh;
+    overflow: auto;
+}
+
 .brand {
     font-size: 10em;
     color: #222;
     font-family: "Anton";
-    border-bottom: 2px solid #222;
+    border-bottom: 3px solid #222;
+    cursor: pointer;
 }
 
 .seasonsClass {
     font-size: 2em;
     color: #222;
     font-family: "Anton";
+    border-bottom: 2px solid #222;
+    cursor: pointer;
+}
+
+.matchClass {
+    color: #222;
+    font-family: "Anton";
     border-bottom: 1px solid #222;
     cursor: pointer;
 }
 
-.seasonsClass:hover {
+.seasonsClass:hover, .matchClass:hover {
     color: #111;
 }
 </style>
