6749571c84ec2f9914bc8fdb5e10481d3d2fb25b
Shubham Singh <mynameshubham@gmail.com>
2018-03-03 08:25:49 +0400

    chsrtl

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,6 +17,7 @@
                 </div>
             </div>
             <div class="col-sm-12 col-md-10" style="background-color:  #222; min-height: 100vh;">
+                <canvas id="line-chart" width="800" height="450"></canvas>
             </div>
         </div>
     </div>
@@ -28,6 +29,10 @@
 <script>
 
 import Papa from '../../node_modules/papaparse/papaparse.js'
+
+window.onload = function() {
+
+}
 
 
 //hard-coding the size of matches array to 10x4
@@ -78,7 +83,7 @@ fetch('https://f1zqng.bn.files.1drv.com/y4mjnTFba2iSF--66P1CdTG2NqjIJp1vGMjfDgeo
     }
     response.text().then(function(data) {
         mySeasons = Papa.parse(data).data;
-        mySeasons.splice(mySeasons.length-1, 1);
+        mySeasons.splice(mySeasons.length - 1, 1);
     });
 }).catch(function(err) {
     console.log('Fetch Error :-S', err);
@@ -106,7 +111,48 @@ setTimeout(function() {
     seasons.splice(0, 1);
     seasons.slice().reverse();
 
-}, 2000);
+    new Chart(document.getElementById("line-chart"), {
+        type: 'line',
+        data: {
+            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            datasets: [{
+                data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                label: "Africa",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+                label: "Asia",
+                borderColor: "#8e5ea2",
+                fill: false
+            }, {
+                data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+                label: "Europe",
+                borderColor: "#3cba9f",
+                fill: false
+            }, {
+                data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+                label: "Latin America",
+                borderColor: "#e8c3b9",
+                fill: false
+            }, {
+                data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+                label: "North America",
+                borderColor: "#c45850",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'World population per region (in millions)'
+            }
+        }
+    });
+
+
+}, 1000);
 
 export default {
 
